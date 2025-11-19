5dd7d2f306759956ee0d9b1649554a37ff6e0e5a
Kifansa <kifansafadhlurrohman5@gmail.com>
2025-11-19 12:15:13 +0700

    Setup DB Connection, Create User Table, Struktur Basic Flutter

---
+++ lapor_balongmojo_api/src/config/database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+const dbPromise = db.promise();
+
+module.exports = dbPromise;
