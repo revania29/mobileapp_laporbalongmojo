287d792245282ad4fc831567160687cc68ce77ce
Kifansa <kifansafadhlurrohman5@gmail.com>
2025-12-17 21:51:17 +0700

    Setup Infrastruktur Firebase(Frontend & Backend)

--- lapor_balongmojo/android/app/build.gradle.kts
+++ lapor_balongmojo/android/app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +41,10 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    implementation (platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation ("com.google.firebase:firebase-analytics")
 }
